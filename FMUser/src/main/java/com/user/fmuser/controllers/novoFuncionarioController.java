52b952f1aa0e6e0174a4c1173404cc28b8f85cf3
Izadora Netz <izadora.ns@gmail.com>
2025-11-30 20:33:34 -0300

    implementando cadastramento de funcionário

--- FMUser/src/main/java/com/user/fmuser/controllers/novoFuncionarioController.java
+++ FMUser/src/main/java/com/user/fmuser/controllers/novoFuncionarioController.java
@@ -1,15 +1,14 @@
 package com.user.fmuser.controllers;
 
+import com.user.fmuser.models.Database;
+import com.user.fmuser.models.Funcionario;
 import com.user.fmuser.MainApplication;
 import com.user.fmuser.utils.ScreenManager;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import java.sql.SQLException;
 
 public class novoFuncionarioController {
-
-    protected static String texto = "Selecionar";
-
-    public static int cargoFuncionario = 0;
 
     public void logout() {
         MainApplication.logout();
@@ -21,8 +20,40 @@ public class novoFuncionarioController {
         ScreenManager.getInstance().showScreen("/com/user/fmuser/funcionarios-view.fxml", "Funcionarios");
     }
 
+    protected static String texto = "Selecionar";
+
+    public static int cargoFuncionario = 0;
+
+    @FXML
+    private TextField cpfField;
+
+    @FXML
+    private TextField nomeField;
+
+    @FXML
+    private TextField sobrenomeField;
+
+    @FXML
+    private Label cpfMessage;
+
+    @FXML
+    private Label cargoMessage;
+
+    @FXML
+    private Label nomeMessage;
+
+    @FXML
+    private Label sobrenomeMessage;
+
+    @FXML
+    private Button cadastrarButton;
+
+    @FXML
+    private Button cancelarButton;
+
     @FXML
     public ComboBox<String> cargoFuncionarioMenu;
+
 
     @FXML
     public void initialize(){
@@ -38,16 +69,35 @@ public class novoFuncionarioController {
             texto = null;
         }
         cargoFuncionarioMenu.setValue(texto);
+
+        //inicializa com mensagens ocultas
+        cpfMessage.setVisible(false);
+        cargoMessage.setVisible(false);
+        nomeMessage.setVisible(false);
+        sobrenomeMessage.setVisible(false);
     }
 
 
 
     @FXML
     public void handleCargoFuncionario() {
+        String value = cargoFuncionarioMenu.getValue();
+        if("Motorista".equals(value)){
+            cargoFuncionario = 1;
+        } else if ("Cobrador".equals(value)) {
+            cargoFuncionario = 2;
+        } else {
+            cargoFuncionario = 0;
+        }
 
+        if(cargoMessage !=null ){
+            cargoMessage.setVisible(cargoFuncionario == 0);
+        }
     }
     @FXML
-    public void handleCadastrarFuncionario() {}
+    public void handleCadastrarFuncionario() {
+
+    }
 
     @FXML
     public void verificarCPF() {}
