460c51d256f970a9e2802e4c8b2792f8f1b257dc
Nirva Neves <nive.yellow@gmail.com>
2025-11-30 20:28:25 -0300

    classe Avaliacao

---
+++ FMUser/src/main/java/com/user/fmuser/models/Avaliacao.java
@@ -0,0 +1,8 @@
+package com.user.fmuser.models;
+
+public class Avaliacao {
+    public enum TargetType {Ciclovia, Parada, Viagem};
+    public int codigoAlvo; // Rating target's code
+    public String texto;
+    public String cpfUsuario;
+}
