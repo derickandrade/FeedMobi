e16cfa6b4b274790a4c6834a4c3b8ba2c8c44f0d
Derick <derickdaniel.andrade@gmail.com>
2025-11-29 21:38:09 -0300

    add funcionalidades de cadastro

--- FMUser/src/main/java/com/user/fmuser/controllers/CadastroController.java
+++ FMUser/src/main/java/com/user/fmuser/controllers/CadastroController.java
@@ -1,10 +1,10 @@
 package com.user.fmuser.controllers;
 
+import com.user.fmuser.models.Database;
+import com.user.fmuser.models.Usuario;
 import com.user.fmuser.utils.ScreenManager;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class CadastroController {
 
@@ -15,6 +15,7 @@ public class CadastroController {
     private String nome;
     private String sobrenome;
     private boolean isAdmin;
+    boolean cpfDisponivel = false;
 
     @FXML
     private Button voltarButton;
@@ -41,6 +42,27 @@ public class CadastroController {
     private PasswordField senhaField;
 
     @FXML
+    private Label cpfMessage;
+
+    @FXML
+    private Label emailMessage;
+
+    @FXML
+    private Label nomeMessage;
+
+    @FXML
+    private Label sobrenomeMessage;
+
+    @FXML
+    private Label senhaMessage;
+
+    @FXML
+    private Label confirmarSenhaMessage;
+
+    @FXML
+    private Label senhaConfereMessage;
+
+    @FXML
     protected void retornarLogin() {
         ScreenManager.getInstance().showScreen("/com/user/fmuser/login-view.fxml", "Login");
     }
@@ -49,5 +71,78 @@ public class CadastroController {
     public void handleCadastro() {
         cpf = cpfField.getText();
         email = emailField.getText();
+        senha = senhaField.getText();
+        confirmarSenha = confirmarSenhaField.getText();
+        nome = nomeField.getText();
+        sobrenome = sobrenomeField.getText();
+        if (cpf.isEmpty()) {
+            cpfMessage.setStyle("-fx-text-fill: red");
+            cpfMessage.setText("Insira seu CPF!");
+            cpfMessage.setVisible(true);
+        }
+
+        boolean emailValido = !email.isEmpty();
+        boolean nomeValido = !nome.isEmpty();
+        boolean sobrenomeValido = !sobrenome.isEmpty();
+        boolean senhaValida = !senha.isEmpty();
+        boolean confirmarSenhaValida = !confirmarSenha.isEmpty();
+        boolean senhasConferem = senha.equals(confirmarSenha);
+
+        emailMessage.setVisible(!emailValido);
+        nomeMessage.setVisible(!nomeValido);
+        sobrenomeMessage.setVisible(!sobrenomeValido);
+        senhaMessage.setVisible(!senhaValida);
+        confirmarSenhaMessage.setVisible(!confirmarSenhaValida);
+        senhaConfereMessage.setVisible(!senhasConferem);
+
+        if (emailValido && nomeValido && sobrenomeValido && senhaValida && confirmarSenhaValida && senhasConferem) {
+            Usuario usuario = new Usuario(cpf, nome, sobrenome, email, senha);
+
+            try {
+                Database.connect();
+                Database.adicionaUsuario(usuario);
+                Database.disconnect();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("");
+                alert.setHeaderText("Cadastro sucedido!");
+                alert.setContentText("Realize o login.");
+                alert.showAndWait();
+                retornarLogin();
+            } catch (RuntimeException e) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("ERRO");
+                alert.setHeaderText("Não foi possível realizar o cadastro!");
+                alert.setContentText("Ocorreu um erro: \n" + e.toString() + "\nTente novamente.");
+                alert.showAndWait();
+            }
+
+        }
+    }
+
+    @FXML
+    public void verificarCPF() {
+        cpf = cpfField.getText();
+        System.out.println(cpf.length());
+        if (cpf.length() == 11) {
+            cpfDisponivel = verificarCPFnoBD(cpf);
+            if (cpfDisponivel) {
+                cpfMessage.setStyle("-fx-text-fill: green;");
+                cpfMessage.setText("CPF disponível!");
+                cpfMessage.setVisible(true);
+            }
+            else {
+                cpfMessage.setStyle("-fx-text-fill: red;");
+                cpfMessage.setText("CPF já cadastrado!");
+                cpfMessage.setVisible(true);
+            }
+        }
+    }
+
+    public boolean verificarCPFnoBD(String cpf) {
+        Database.connect();
+        boolean cpfDisponivel = !(Database.cpfCadastrado(cpf));
+        Database.disconnect();
+        System.out.println(cpfDisponivel);
+        return cpfDisponivel;
     }
 }
